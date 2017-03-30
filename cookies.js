6c0808b4db8a37272709ba24edcd72444cde5d70
Vincent Martorano <martoranov94@gmail.com>
2017-03-30 13:24:14 -0400

    functoinal according to directions and extra total count that doesnt work

--- cookies.js
+++ cookies.js
@@ -1,28 +1,54 @@
 var sugarBtn = document.querySelector("#sugar-button")
 var chocBtn = document.querySelector("#chocolate-button")
 var lemonBtn = document.querySelector("#lemon-button")
-var sugar = 0
-var chocolate = 0
-var lemon = 0
-var cookies = 0
+var clearBtn = document.querySelector("#clear-button")
 
-sugarBtn.addEventListener("click", function(){
-  sugar++
-  Cookies.set("sugar", sugar);
-  var sCount = document.querySelector("#sCount")
-  sCount.textContent = "Sugar Cookies: " + sugar
+if (document.cookie.indexOf('sugar') == -1) {
+    Cookies.set('sugar', 0)
+  }
+
+if (document.cookie.indexOf('chocolate') == -1) {
+    Cookies.set('chocolate', 0)
+  }
+
+if (document.cookie.indexOf('lemon') == -1) {
+    Cookies.set('lemon', 0)
+  }
+
+clearBtn.addEventListener("click", function(event){
+  Cookies.set('sugar', document.querySelector("#sCount").innerHTML = 0)
+  Cookies.set('chocolate', document.querySelector("#cCount").innerHTML = 0)
+  Cookies.set('lemon', document.querySelector("#lCount").innerHTML = 0)
+  })
+
+document.getElementById('sugar-clicks').innerHTML = Cookies.get("sugar");
+
+sugarBtn.addEventListener("click", function(event){
+  sugarNumber = Cookies.get("sugar")
+  sugarI = parseInt(sugarNumber)
+  sugarI ++
+  Cookies.set("sugar", sugarI);
+  document.querySelector("#sCount").innerHTML = sugarI
 })
 
-chocBtn.addEventListener("click", function(){
-  chocolate++
-  Cookies.set("chocolate", chocolate);
-  var cCount = document.querySelector("#cCount")
-  cCount.textContent = "Chocolate Cookies: " + chocolate
+document.getElementById('chocolate-clicks').innerHTML = Cookies.get("chocolate");
+
+chocBtn.addEventListener("click", function(event){
+  chocolateNumber = Cookies.get("chocolate")
+  chocolateI = parseInt(chocolateNumber)
+  chocolateI ++
+  Cookies.set("chocolate", chocolateI);
+  document.querySelector("#cCount").innerHTML = chocolateI
 })
 
-lemonBtn.addEventListener("click", function(){
-  lemon++
-  Cookies.set("lemon", lemon);
-  var lCount = document.querySelector("#lCount")
-  lCount.textContent = "Lemon Cookies: " + lemon
+document.getElementById('lemon-clicks').innerHTML = Cookies.get("lemon");
+
+lemonBtn.addEventListener("click", function(event){
+  lemonNumber = Cookies.get("lemon")
+  lemonI = parseInt(lemonNumber)
+  lemonI ++
+  Cookies.set("lemon", lemonI);
+  document.querySelector("#lCount").innerHTML = lemonI
 })
+
+// document.getElementById('total-clicks').innerHTML = parseInt(document.querySelector("#sugar-clicks")) + parseInt(document.querySelector("#chocolate-clicks")) + parseInt(document.querySelector("#lemon-clicks"))
